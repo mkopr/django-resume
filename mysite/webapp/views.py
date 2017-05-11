9d69d77ba2b6a217c7c20b04589950ab98bcac92
Marcin Koprek <marcinkoprek@gmail.com>
2017-05-11 16:04:25 +0200

    PEP8

--- mysite/webapp/views.py
+++ mysite/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     simple_http_code = '<h2>Wiktor to gejaszek!</h2>'
     return HttpResponse(simple_http_code)
