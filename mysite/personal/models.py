01f4e49ede6dae719af70cbb722c905f61cca63c
Marcin Koprek <marcinkoprek@gmail.com>
2017-05-19 21:35:00 +0200

    Add text for HOME in data base.

--- mysite/personal/models.py
+++ mysite/personal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class WelcomeText(models.Model):
+    text = models.TextField()
+    add_date = models.DateField()
+
+    def __str__(self):
+        return self.add_date
