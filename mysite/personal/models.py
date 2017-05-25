f9b219ac1913164a348ad0f27f2bfeff793822d5
Marcin Koprek <marcinkoprek@gmail.com>
2017-05-25 13:57:08 +0200

    Add data from data base to html code.

--- mysite/personal/models.py
+++ mysite/personal/models.py
@@ -3,10 +3,9 @@ from django.db import models
 
 class WelcomeText(models.Model):
     text = models.TextField()
-    add_date = models.DateField()
 
     def __str__(self):
-        return self.add_date
+        return self.text
 
 
 class Academics(models.Model):
--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
+from personal.models import WelcomeText, Academics, BasicInfo, Experience, \
+    Projects, Skills
 
 
 def index(request):
     personal_html = 'personal/home.html'
-    return render(request, personal_html)
+    welcome_text = WelcomeText.objects.first()
+    academics = Academics.objects.first()
+    basic_info = BasicInfo.objects.get(first_name='Marcin')
+    experience_1 = Experience.objects.get(company_name='Delivery Service')
+    experience_2 = Experience.objects.get(company_name='Law Office')
+    skills = Skills.objects.first()
+    project_1 = Projects.objects.get(name='morg')
+    project_2 = Projects.objects.get(name='django-resume')
+
+    return render(
+        request=request,
+        template_name=personal_html,
+        context={
+            'welcome_text': welcome_text,
+            'basic_info': basic_info,
+            'academics': academics,
+            'experience_1': experience_1,
+            'experience_2': experience_2,
+            'skills': skills,
+            'project_1': project_1,
+            'project_2': project_2,
+        },
+    )
